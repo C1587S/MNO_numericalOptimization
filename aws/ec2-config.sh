9131e3e5bf93b8ef1247038e872d8fc42852ad2e
Pilo1961 <31834229+Pilo1961@users.noreply.github.com>
2020-05-15 11:02:10 -0500

    borra comentarios

--- aws/ec2-config.sh
+++ aws/ec2-config.sh
@@ -2,33 +2,18 @@
 
 #instalacio de paquetes
 apt-get update
-#apt-get install docker-ce docker-ce-cli containerd.io
 apt-get install git
 apt install docker.io
 
 #creacion de usuarios
 adduser --gecos --disabled-login --force-badname "JP"
-#usermod -p 'saoW8Z/xz5qko' JP
 usermod -aG sudo JP
 echo "User created"
 
 adduser --gecos --disabled-login --force-badname "Dani"
-#usermod -p 'saoW8Z/xz5qko' Dani
 usermod -aG sudo Dani
 echo "User Created"
 
 adduser --gecos --disabled-login --force-badname "Sebastian"
-#usermod -p 'saoW8Z/xz5qko' Sebastian
 usermod -aG sudo Sebastian
 echo "User created"
-
-#creacion folders de llaves
-#su JP
-#mkdir /home/JP/.ssh
-#chmod 770 /home/JP/.ssh
-#su Sebastian
-#mkdir /home/Sebastian/.ssh
-#chmod 770 /home/Sebastian/.ssh
-#su Dani
-#mkdir home/Dani/.ssh
-#chmod 770 home/Dani/.ssh
